09c73dd1c7ba1bce36ca0cd3a0dc26346daa9eff
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:22:48 +0200

    Exemple

---
+++ io-age-calculator.py
@@ -0,0 +1,9 @@
+#calculating python's age
+
+current_year = input("Waht year is it ?")       # hardcoding
+current_year = int (current_year)
+release_year = 1991
+
+age = current_year - release_year                 # age -> integer
+
+print( "Python Language Age is:",age,"years" )    #"abc" - string
